a6c8c7123f8637b725be7f3f419420ec5734b03c
indydutch <55841244+indydutch@users.noreply.github.com>
2019-10-02 11:18:20 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,27 +2,27 @@
 
 using namespace std;
 
-void GCDFunc(long long int A, long long int B){
+void GCDFunc(long long int A, long long int B){ //function for find the GCD
 
-    long long int r, GCD;
+    long long int r, GCD; //r means remainder
 
-    while (r > 0){
+    while (r > 0){ //loops while r is greater than 0
         if (A > B){
-            r = A % B;
+            r = A % B; //makes r equal to the remainder of A divided by B
 
-            A = B;
-            B = r;
-            GCD = A;
+            A = B; //replaces A with B
+            B = r; //replaces B with r
+            GCD = A; //replaces GCD with A
         }
-        else {
-            r = B % A;
+        else { //else statement for if B is bigger than A
+            r = B % A; 
 
             B = A;
             A = r;
             GCD = B;
         }
     }
-    cout << GCD << endl;
+    cout << "The greatest common divisor of the two numbers is: " << GCD << endl; //prints the GCD
 }
 
 int main(){
@@ -30,12 +30,12 @@ int main(){
     long long int A, B;
 
     cout << "Input the first common divisor number: " << endl;
-    cin >> A;
+    cin >> A; //gets a user input for A
     cout << "Input the second common divisor number: " << endl;
-    cin >> B;
+    cin >> B; //gets a user input for B
     cout << "\n";
 
-    GCDFunc(A, B);
+    GCDFunc(A, B); //calls the GCD function
 
 return 0;
 }
