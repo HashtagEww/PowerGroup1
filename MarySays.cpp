db2963f87924539e01f5a95ee7fd8a1ff7196564
indydutch <55841244+indydutch@users.noreply.github.com>
2019-10-31 09:13:47 -0400

    Update MarySays.cpp

--- MarySays.cpp
+++ MarySays.cpp
@@ -1,16 +1,52 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main()
-{
-    
-    
-    
-    
-    
-    
-    
-    
+class MarySays {
+private:
+    string lastCommand;
+    int gameRound;
+public:
+    MarySays(){
+        lastCommand = "Jump in the Air";
+        gameRound = 1;
+    }
+    MarySays(string newCommand){
+        lastCommand = newCommand;
+        gameRound = 1;
+    }
+    void sayCommands(string s){
+        lastCommand = s;
+        gameRound++;
+    }
+    void display(){
+        cout << lastCommand << endl;
+        cout << gameRound << endl;
+        cout << "\n";
+    }
+};
+
+int main(){
+    MarySays testObj;
+    MarySays testObj2;
+    testObj.display();
+
+    string command1;
+    cout << "Please enter a command:" << endl;
+    cin >> command1;
+    cout << "\n";
+    testObj2.sayCommands(command1);
+    testObj2.display();
+
+    string command2 = "Test1";
+    string command3 = "Test2";
+    string command4 = "Test3";
+    testObj.sayCommands(command2);
+    testObj.sayCommands(command3);
+    testObj2.sayCommands(command4);
+    testObj.display();
+    testObj2.display();
+
     return 0;
 }
